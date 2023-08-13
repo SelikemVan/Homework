f79482cdc1b3db52a142618b1fa74c12845d7010
SelikemVan <selikemvan@gmail.com>
2023-08-13 15:16:43 +0100

    First commit

--- Temperature_Converter.py
+++ Temperature_Converter.py
@@ -1,5 +1,25 @@
 def convert_temperature(temperature, unit_from, unit_to):
 
+    """This function converts temperature from one unit to another.
+
+    :arguments:
+        temperature: The temperature to be converted.
+        unit_from: The unit of the temperature to be converted.
+        unit_to: The unit to convert to.
+
+    Returns:
+        The converted temperature.
+    """
+
+    # Check if the unit from and unit to are valid.
+
+    if unit_from not in ["Celsius", "Fahrenheit", "Kelvin"]:
+        raise ValueError("Invalid unit from!")
+    if unit_to not in ["Celsius", "Fahrenheit", "Kelvin"]:
+        raise ValueError("Invalid unit to!")
+
+    # Calculate the converted temperature.
+
     if unit_from == "Celsius":
         if unit_to == "Fahrenheit":
             return temperature * 9 / 5 + 32
@@ -19,7 +39,8 @@ def convert_temperature(temperature, unit_from, unit_to):
 
 def main():
 
-    temperature = input("Enter the temperature: ")
+    temperature = float(input("Enter the temperature: "))
+    print("What unit should the temperature be? For example, Celsius, Fahrenheit, and Kelvin")
     unit_from = input("Enter the unit of the temperature: ")
     unit_to = input("Enter the unit to convert to: ")
     try:
@@ -29,4 +50,4 @@ def main():
     converted_temperature = convert_temperature(temperature, unit_from, unit_to)
     print("The converted temperature is:", converted_temperature)
 
-main()
+    main()
